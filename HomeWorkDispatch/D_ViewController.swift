151f0ae2c54fa9eb8ef6be099d132e30136d0534
Igor Frik <murlocov@icloud.com>
2022-08-20 02:36:26 +0700

    Roll

--- HomeWorkDispatch/D_ViewController.swift
+++ HomeWorkDispatch/D_ViewController.swift
@@ -7,9 +7,39 @@
 
 import UIKit
 
+class LetMeDoIt {
+    let serialQueue = DispatchQueue(label: "personal.serial.background", qos: .background)
+    
+
+    func addIt(_ new: ()) {
+        serialQueue.sync {
+            new
+        }
+    }
+}
+
 class D_ViewController: UIViewController {
+    
+    var count = 10
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        func test() {
+            let number = count
+            var array = [1]
+            for i in 2...number {
+                if i > 1 && !(2..<i).contains(where: { i % $0 == 0 }) { array.append(i)}
+            }
+            print("Найдено \(array.count) простых чисел: \(array)")
+            self.count *= 10
+        }
+        
+        let a = LetMeDoIt()
+        a.addIt(test())
+        a.addIt(test())
+        a.addIt(test())
+        a.addIt(test())
+        a.addIt(test())
     }
 }
