c712d686098b4ee08d988cab1e4c12d57c309420
Igor Frik <murlocov@icloud.com>
2022-08-20 00:31:53 +0700

    a & b

--- HomeWorkDispatch/ViewController.swift
+++ HomeWorkDispatch/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var aImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            let image = UIImage(data: try! Data(contentsOf: URL(string: "https://img2.joyreactor.cc/pics/post/never-gonna-give-you-up-rickroll-6650260.jpeg")!))
+            DispatchQueue.main.async { [weak self] in
+                self?.aImageView.image = image
+            }
+        }
     }
-
-
 }
 
---
+++ HomeWorkDispatch/bViewController.swift
@@ -0,0 +1,29 @@
+//
+//  bViewController.swift
+//  HomeWorkDispatch
+//
+//  Created by Igor Frik on 20.08.2022.
+//
+
+import UIKit
+
+class bViewController: UIViewController {
+
+    @IBOutlet weak var bImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterial))
+        blurEffectView.frame = bImageView.frame
+        
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            let image = UIImage(data: try! Data(contentsOf: URL(string: "https://imgs.smoothradio.com/images/191589?width=1200&crop=1_1&signature=KHg-WnaLlH9KsZwE-qYgxTkaSpU=")!))
+            DispatchQueue.main.async { [weak self] in
+                self?.bImageView.addSubview(blurEffectView)
+                self?.bImageView.image = image
+            }
+        }
+    }
+
+}
