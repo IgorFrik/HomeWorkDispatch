6b8069c0f9779a4755fc17f3ffc4d7096197f2e9
Igor Frik <murlocov@icloud.com>
2022-08-20 01:39:31 +0700

    c ready

---
+++ HomeWorkDispatch/cViewController.swift
@@ -0,0 +1,36 @@
+//
+//  cViewController.swift
+//  HomeWorkDispatch
+//
+//  Created by Igor Frik on 20.08.2022.
+//
+
+import UIKit
+
+extension Int {
+    func powerOf2() -> Int {
+        return self * self
+    }
+}
+
+class cViewController: UIViewController {
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func run(_ sender: Any) {
+        guard let number = Int(numberTextField.text!) else { return }
+        DispatchQueue.global(qos: .background).async {
+            var array = [1]
+            let startTime = Date()
+            for i in 2...number {
+                if i > 1 && !(2..<i).contains(where: { i % $0 == 0 }) { array.append(i)}
+            }
+            let overallTime = Date()
+            print("Найдено \(array.count) простых чисел: \(array)")
+            print("Всего за \(String(format: "%.1f", overallTime.timeIntervalSince(startTime))) c.")
+        }
+    }
+}
